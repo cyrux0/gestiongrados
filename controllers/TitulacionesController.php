97971e695f7ac41b8097ec781b48b80c05ae52b6
Daniel Salazar <danielsalazarrecio@gmail.com>
2011-03-17 18:46:46 +0100

    Añadida carpeta controladores con: -TitulacionesController -ApplicationController

---
+++ controllers/TitulacionesController.php
@@ -0,0 +1,33 @@
+﻿<?php
+
+class TitulacionesController
+{
+	function __construct()
+	{
+		//Instancia del motor de vistas
+		$this->view = new View();
+	
+	}
+	
+	public function index(){
+		require 'models/titulacionesModel.php';
+		
+		//Instancia del modelo
+		$titulaciones = new TitulacionesModel();
+		
+		//Conseguimos los items
+		$array_titulaciones = $titulaciones->listadoTotal();
+		
+		//Pasamos a la vista los datos en el array data
+		$data['titulaciones'] = $array_titulaciones;
+		$this->view->show("indexTitulaciones.php", $data);
+		
+	}
+	
+	public function add()
+	{
+		echo "En construcción: aquí iría el formulario para nuevas titulaciones";
+	}
+}
+
+?>
---
+++ controllers/applicationController.php
@@ -0,0 +1,40 @@
+﻿<?php
+	class ApplicationController{
+		static function main(){
+			//Incluimos clases
+			
+			require 'libs/config.php';
+			require 'libs/SPDO.php';
+			require 'libs/view.php';
+			
+			require 'config.php';
+			
+			//Formamos el nombre del controlador por defecto o el que se pida
+			if(!empty($_GET['controller']))
+				$controllerName = $_GET['controller'] . 'Controller';
+			else
+				$controllerName = "TitulacionesController";
+			
+			if(!empty($_GET['action']))
+				$actionName = $_GET['action'];
+			else
+				$actionName = "index";
+			
+			$controllerPath = $config->get('controllersFolder') . $controllerName . '.php';
+			
+			if(is_file($controllerPath))
+				require $controllerPath;
+			else
+				die('El controlador no existe - 404 not found ' . $controllerPath );
+			
+			if(!is_callable(array($controllerName, $actionName))){
+				trigger_error($controllerName . '->' . $actionName . ' no existe', E_USER_NOTICE);
+				return false;
+			}
+		
+			$controller = new $controllerName();
+			$controller->$actionName();
+		}
+	}
+	
+?>
