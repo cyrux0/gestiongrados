d708beec8823afe25630a9ecfa709453c3d9fcd4
Daniel Salazar <danielsalazarrecio@gmail.com>
2011-03-19 14:37:14 +0100

    Modificado modelo de Titulaciones con función de añadir elemento, aun sin probar, el resto funciona perfectamente

--- models/titulacionesModel.php
+++ models/titulacionesModel.php
@@ -11,5 +11,14 @@ class TitulacionesModel extends ModelBase
     //Devolvemos la colección para pasarsela al controlador o vista
     return $consulta;	
   }
+
+  public function newelem($params){
+    $paramsnames = implode(",", array_keys($params));
+    $paramscolon = implode(",", array_map(create_function('$a','return ":".$a;'),array_keys($params)));
+    
+    $sql = "INSERT INTO titulaciones (".$paramsnames.") VALUES (".$paramscolon.")";
+    $query = $this->db->prepare($sql);
+    $query->execute();
+  }
 }
 ?>
