639aa305413304691489c152de98a5bb1bcd84a3
Daniel Salazar <danielsalazarrecio@gmail.com>
2011-03-26 13:02:08 +0100

    Movido contenido directorio codeigniter a la raiz del proyecto

---
+++ application/controllers/titulaciones.php
@@ -0,0 +1,13 @@
+<?php
+class Titulaciones_Controller extends CI_Controller{
+  public function index(){
+    //Conseguimos los items mediante el modelo
+    
+    //Pasamos los datos a la vista
+
+    //Mostramos
+    
+  }
+}
+
+?>
