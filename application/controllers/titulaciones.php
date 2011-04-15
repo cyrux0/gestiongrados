4957d43e53ba4d5f2ad4e2d5372bf0b93c7b9e0b
Daniel Salazar <danielsalazarrecio@gmail.com>
2011-04-15 12:35:09 +0200

    Actualizado titulaciones para usar Doctrine 1.2, funcionando todo salvo el enlace con Asignaturas

--- application/controllers/titulaciones.php
+++ application/controllers/titulaciones.php
@@ -1,22 +1,17 @@
 <?php
 class Titulaciones extends CI_Controller{
   
-  private $em;
-  private $titulacionRepo;
-
   function __construct(){
     parent::__construct();
-    $this->load->model('Titulacion');
-    $this->load->model('Asignatura_model');
-    $this->em = $this->doctrine->em;
-    $this->titulacionRepo = $this->em->getRepository('Titulacion');
+    $this->titulaciones_table = Doctrine::getTable('Titulacion');
+    $this->asignaturas_table = Doctrine::getTable('Asignatura');
   }
   
   public function index(){
-    //Cargamos el modelo
-    
+    $titulaciones = $this->titulaciones_table->findAll();
+
     //Conseguimos los items mediante el modelo
-    $data['titulaciones'] = $this->titulacionRepo->findAll();
+    $data['titulaciones'] = $titulaciones;
 
     //Mostramos
     
@@ -25,48 +20,45 @@ class Titulaciones extends CI_Controller{
 
   public function add(){
     //Mostramos vista
-    $titulacion = new Titulacion;
+    $titulacion = new Titulacion();
     
     $this->load->view('titulaciones/add', array('data' => array('titulacion' => $titulacion)));
   }
 
   public function create(){
     $titulacion = new Titulacion;
-    $titulacion->creditos = $_POST['creditos'];
-    $titulacion->nombre = $_POST['nombre'];
-    $titulacion->codigo = $_POST['codigo'];
+    $titulacion->creditos = $this->input->post('creditos');
+    $titulacion->nombre = $this->input->post('nombre');
+    $titulacion->codigo = $this->input->post('codigo');
     $titulacion->save();
     redirect('titulaciones/index');
   }
 
   public function delete($id){
-    
-    $titulacion = $this->titulacionRepo->find($id);
-    $this->em->remove($titulacion);
-    $this->em->flush();
+    $titulacion = $this->titulaciones_table->find($id);
+    $titulacion->delete();
     redirect('titulaciones/index');
   }
 
   public function edit($id){
-    $result = $this->titulacionRepo->find($id);
-    $data['titulacion'] = $result;
+    $titulacion = $this->titulaciones_table->find($id);
+    $data['titulacion'] = $titulacion;
    
     $this->load->view('titulaciones/edit', $data);
   }
 
   public function update($id){
-    $titulacion = $this->titulacionRepo->find($id);
-    $titulacion->codigo = $_POST['codigo'];
-    $titulacion->nombre = $_POST['nombre'];
-    $titulacion->creditos = $_POST['creditos'];
-    $this->em->persist($titulacion);
-    $this->em->flush();
+    $titulacion = $this->titulaciones_table->find($id);
+    $titulacion->codigo = $this->input->post('codigo');
+    $titulacion->nombre = $this->input->post('nombre');
+    $titulacion->creditos = $this->input->post('creditos');
+    $titulacion->save();
     redirect('titulaciones/index');
   }
 
   public function show($id){
-    $data['asignaturas'] = $this->Asignatura_model->find_by_titulacion($id);
-    $data['titulacion'] = $this->titulacionRepo->find($id);
+    $data['asignaturas'] = $this->asignaturas_table->findByTitulacion_id($id);
+    $data['titulacion'] = $this->titulaciones_table->find($id);
     
     $this->load->view('titulaciones/show', $data);
   }
