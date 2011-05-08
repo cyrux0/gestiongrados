8099c818a544cadeb5ddca3a16c4c2c171cb1d28
Daniel Salazar <danielsalazarrecio@gmail.com>
2011-05-08 21:18:32 +0200

    Añadido el create a cargasglobales, ya se pueden crear sin problemas, ahora faltaría enlazarlo

--- application/controllers/cargasglobales.php
+++ application/controllers/cargasglobales.php
@@ -15,4 +15,31 @@ class CargasGlobales extends CI_Controller{
     $data['nombre_asignatura'] = $this->asignaturas_table->find($asignatura_id)->nombre;
     $this->load->view('cargaglobal/add', $data);
   }
+
+  public function create(){
+    $global = new CargaGlobal;
+    $global->horas_teoria = $this->input->post('horas_teoria');
+    $global->creditos_teoria = $this->input->post('creditos_teoria');
+    $global->grupos_teoria = $this->input->post('grupos_teoria');
+    $global->horas_lab = $this->input->post('horas_lab');
+    $global->creditos_lab = $this->input->post('creditos_lab');
+    $global->grupos_lab = $this->input->post('grupos_lab');
+    $global->horas_problemas = $this->input->post('horas_problemas');
+    $global->creditos_problemas = $this->input->post('creditos_problemas');
+    $global->grupos_problemas = $this->input->post('grupos_problemas');
+    $global->horas_informatica = $this->input->post('horas_informatica');
+    $global->creditos_informatica = $this->input->post('creditos_informatica');
+    $global->grupos_informatica = $this->input->post('grupos_informatica');
+    $global->horas_campo = $this->input->post('horas_campo');
+    $global->creditos_campo = $this->input->post('creditos_campo');
+    $global->grupos_campo = $this->input->post('grupos_campo');
+    
+    $asignatura = $this->asignaturas_table->find($this->input->post('asignatura'));
+    $asignatura->CargasGlobales[] = $global;
+    $asignatura->save();
+    $global->save();
+    redirect('titulaciones/index');
+  }
+  
+
 }
