8c9635ae8169a2890e1007373f905cb032c716f1
Daniel Salazar <danielsalazarrecio@gmail.com>
2011-05-09 21:09:27 +0200

    Añadido edit y update a carga global, faltaría borrar y refinar. [refs #2]

--- application/controllers/cargasglobales.php
+++ application/controllers/cargasglobales.php
@@ -10,7 +10,7 @@ class CargasGlobales extends CI_Controller{
   public function add($asignatura_id){
     $global = new CargaGlobal;
     $global->asignatura_id = $asignatura_id;
-    $action = 'cargasglobales/create/'.$global->asignatura_id;
+    $action = 'cargasglobales/create/';
     $data['data'] = array('result' => $global, 'action' => $action);
     $data['nombre_asignatura'] = $this->asignaturas_table->find($asignatura_id)->nombre;
     $this->load->view('cargaglobal/add', $data);
@@ -41,5 +41,36 @@ class CargasGlobales extends CI_Controller{
     redirect('titulaciones/index');
   }
   
+  public function edit($id){
+    $global = $this->globales_table->find($id);
+    $action = '/cargasglobales/update/'.$id;
+    $data['data'] = array('result' => $global, 'action' => $action);
+    $data['nombre_asignatura'] = $this->asignaturas_table->find($global->asignatura_id)->nombre;
+    $this->load->view('cargaglobal/edit', $data);
+  }
+
+   public function update($id){
+    $global = $this->globales_table->find($id);
+    $global->horas_teoria = $this->input->post('horas_teoria');
+    $global->creditos_teoria = $this->input->post('creditos_teoria');
+    $global->grupos_teoria = $this->input->post('grupos_teoria');
+    $global->horas_lab = $this->input->post('horas_lab');
+    $global->creditos_lab = $this->input->post('creditos_lab');
+    $global->grupos_lab = $this->input->post('grupos_lab');
+    $global->horas_problemas = $this->input->post('horas_problemas');
+    $global->creditos_problemas = $this->input->post('creditos_problemas');
+    $global->grupos_problemas = $this->input->post('grupos_problemas');
+    $global->horas_informatica = $this->input->post('horas_informatica');
+    $global->creditos_informatica = $this->input->post('creditos_informatica');
+    $global->grupos_informatica = $this->input->post('grupos_informatica');
+    $global->horas_campo = $this->input->post('horas_campo');
+    $global->creditos_campo = $this->input->post('creditos_campo');
+    $global->grupos_campo = $this->input->post('grupos_campo');
+    
+    $global->save();
+    redirect('titulaciones/index');
+  }
+ 
+ 
 
 }
