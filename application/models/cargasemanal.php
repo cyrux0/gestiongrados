22264c06ef339da588838dc237e983f781c45def
Daniel Salazar <danielsalazarrecio@gmail.com>
2011-05-10 19:38:29 +0200

    Añadido modelo carga semanal. [refs #3]

---
+++ application/models/cargasemanal.php
@@ -0,0 +1,78 @@
+<?php
+//Connection Component Binding
+Doctrine_Manager::getInstance()->bindComponent('CargaSemanal', 'default');
+
+/**
+ * CargaSemanal
+ *
+ * 
+ * @property integer $id
+ *
+ * @author     Daniel Ignacio Salazar Recio <danielsalazarrecio@gmail.com>
+ */
+class CargaSemanal extends Doctrine_Record
+{
+  public function setTableDefinition()
+  {
+    $this->setTableName('cargas_semanales');
+    $this->hasColumn('id', 'integer', 4, array(
+					       'type' => 'integer',
+					       'length' => 4,
+					       'fixed' => false,
+					       'unsigned' => false,
+					       'primary' => true,
+					       'autoincrement' => true,
+					       ));
+    $this->hasColumn('horas_teoria', 'integer', 4, array(
+							 'type' => 'integer',
+							 'length' => 4,
+							 'fixed' => false,
+							 'unsigned' => true,
+							 ));
+    $this->hasColumn('horas_problemas', 'integer', 4, array(
+							 'type' => 'integer',
+							 'length' => 4,
+							 'fixed' => false,
+							 'unsigned' => true,
+							 ));
+    $this->hasColumn('horas_informatica', 'integer', 4, array(
+							 'type' => 'integer',
+							 'length' => 4,
+							 'fixed' => false,
+							 'unsigned' => true,
+							 ));
+    $this->hasColumn('horas_lab', 'integer', 4, array(
+							 'type' => 'integer',
+							 'length' => 4,
+							 'fixed' => false,
+							 'unsigned' => true,
+							 ));
+    $this->hasColumn('horas_campo', 'integer', 4, array(
+							 'type' => 'integer',
+							 'length' => 4,
+							 'fixed' => false,
+							 'unsigned' => true,
+							 ));
+
+    $this->hasColumn('entrega_trabajo', 'boolean');
+
+    $this->hasColumn('examen', 'boolean');
+
+    $this->hasColumn('cargaglobal_id', 'integer', 4, array(
+							  'type' => 'integer',
+							  'length' => 4,
+							  'fixed' => false,
+							  'unsigned' => false));
+
+
+
+  }
+  
+  public function setUp()
+  {
+    $this->hasOne('CargaGlobal as cargasglobales', array('local' => 'cargaglobal_id', 'foreign' => 'id'));
+    // $this->hasMany('CargaSemanal as CargasSemanales', array('local' => 'id', 'foreign' => 'global_id'));
+    parent::setUp();
+  }
+
+}
