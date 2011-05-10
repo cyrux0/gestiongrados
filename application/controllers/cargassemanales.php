26709ef67d536811034517d2cfb4233a0632a228
Daniel Salazar <danielsalazarrecio@gmail.com>
2011-05-10 19:53:01 +0200

    Añadido controlador de cargas semanales. [refs #3]

---
+++ application/controllers/cargassemanales.php
@@ -0,0 +1,20 @@
+<?php
+
+class CargasSemanales extends CI_Controller{
+  
+  function __construct(){
+    parent::__construct();
+    $this->asignaturas_table = Doctrine::getTable('Asignatura');
+  }
+
+  public function add($id_asig){
+
+  }
+
+  function
+
+}
+
+
+
+?>
