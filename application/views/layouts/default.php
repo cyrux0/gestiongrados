c3f4cfcf89088f407c778994bc4b763dd390dbb4
Daniel Salazar <danielsalazarrecio@gmail.com>
2011-05-18 18:20:58 +0200

    Añadido .htaccess para eliminar el index.php de la ruta y añadido fichero css, añadido link al css en el layout.

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <?php echo meta('Content-type', 'text/html; charset=utf-8', 'equiv'); ?>
+    <?php echo link_tag('css/style.css'); ?>
     <title><?php if (isset($page_title)) echo $page_title; else echo 'El título de la web'; ?></title>
   </head>
   <body>
