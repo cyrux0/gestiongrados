f4f70f41223db050ae526990f67547ca31bf0285
Daniel Salazar <danielsalazarrecio@gmail.com>
2011-05-19 20:36:39 +0200

    Mejorando un poco la apariencia. [refs #5]

--- application/views/titulaciones/_form.php
+++ application/views/titulaciones/_form.php
@@ -1,9 +1,17 @@
 <?php echo form_open('titulaciones/create'); ?>
-<form>
-<label for="codigo">Código:</label>
-<input type="text" name="codigo" value="<?= $titulacion->codigo ?>" /><br />
-<label for="nombre">Nombre:</label> <input type="text" name="nombre" value="<?= $titulacion->nombre ?>" /><br />
-<label for="creditos">Créditos:</label> <input type="text" name="creditos" value="<?= $titulacion->creditos ?>" /><br />
-<input type="submit" name="button_action" value="Enviar" /> | <?= anchor('titulaciones/index', 'Cancelar') ?><br />
-
+<div class="field">
+  <label for="codigo">Código:</label>
+  <input type="text" name="codigo" value="<?= $titulacion->codigo ?>" />
+</div>
+<div class="field">
+  <label for="nombre">Nombre:</label>
+  <input type="text" name="nombre" value="<?= $titulacion->nombre ?>" />
+</div>
+<div class="field">
+  <label for="creditos">Créditos:</label>
+  <input type="text" name="creditos" value="<?= $titulacion->creditos ?>" />
+</div>
+<div class="actions">
+  <input type="submit" name="button_action" value="Enviar" /> | <?= anchor('titulaciones/index', 'Cancelar') ?>
+</div>
 <?php echo form_close(); ?>
